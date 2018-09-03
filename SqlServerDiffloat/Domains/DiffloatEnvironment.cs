add7c15b64fd5974fbef47ddc1094b213ec4daa2
masaharu.harada <masaharu.harada@sirocco.team>
2018-09-03 12:40:31 +0900

    :pushpin: sqlpackage.exeの実行環境確認まで実装
    ログ出力はNlogで行う

---
+++ SqlServerDiffloat/Domains/DiffloatEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace SqlServerDiffloat.Domains
+{
+    public class DiffloatEnvironment
+    {
+        private static readonly string BinaryFileName = @"sqlpackage.exe";
+
+        private static readonly string SsdtVisualStudioPath =
+            Path.Join(
+                @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\",
+                BinaryFileName);
+
+        private static readonly string SsdtsqlServerPath =
+            Path.Join(
+            @"C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\",
+            BinaryFileName);
+
+
+        public static string GetSQLServerDataToolsPath()
+        {
+
+            if (File.Exists(SsdtVisualStudioPath))
+            {
+                return SsdtVisualStudioPath;
+            }
+
+            if (File.Exists(SsdtsqlServerPath))
+            {
+                return SsdtsqlServerPath;
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ SqlServerDiffloat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NLog;
+using SqlServerDiffloat.Domains;
+
+namespace SqlServerDiffloat
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            var path = DiffloatEnvironment.GetSQLServerDataToolsPath();
+
+            if (string.IsNullOrEmpty(path))
+            {
+                logger.Error(@"Install SQL Server Data Tools (SSDT) from https://docs.microsoft.com/ja-jp/sql/ssdt/download-sql-server-data-tools-ssdt?view=sql-server-2017 .");
+                Console.ReadLine();
+                return;
+            }
+
+        }
+    }
+}
